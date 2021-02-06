43a78e49d0c5139283beab632df917f6f5b7fd47
Ati <65487762+AtiVassileva@users.noreply.github.com>
2021-02-06 21:45:58 +0200

    Delete MS SQL/Table Relations directory

--- MS SQL/Table Relations/09.PeaksInRila.sql
+++
@@ -1,5 +0,0 @@
-SELECT Mountains.MountainRange, Peaks.PeakName, Peaks.Elevation 
-FROM Mountains
-JOIN Peaks ON Peaks.MountainId = Mountains.Id
-WHERE Mountains.MountainRange = 'Rila'
-ORDER BY Peaks.Elevation DESC
