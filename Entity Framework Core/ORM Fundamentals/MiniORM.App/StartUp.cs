28fb58962ea7d7f239061dcb7205f3da9ce2c756
Ati <65487762+AtiVassileva@users.noreply.github.com>
2021-02-19 19:26:15 +0200

    Add files via upload

---
+++ Entity Framework Core/ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+	using Data;
+    using System.Linq;
+    using Data.Entities;
+
+	public class StartUp
+	{
+        private const string connectionString =
+			"Server=.\\SQLEXPRESS;Database=MiniORM;Integrated Security=True";
+		public static void Main()
+		{
+            var context = new SoftUniDbContext(connectionString);
+			
+			context.Employees.Add(new Employee
+			{
+				FirstName = "Gosho",
+				LastName = "Inserted",
+				DepartmentId = context.Departments.First().Id,
+				IsEmployed = true,
+			});
+
+			var employee = context.Employees.Last();
+			employee.FirstName = "Modified";
+
+			context.SaveChanges();
+		}
+	}
+}
