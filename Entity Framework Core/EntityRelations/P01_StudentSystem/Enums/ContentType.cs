c9daf3b222f645e91c2ae2c1a6f2b8e367e2596c
Ati <65487762+AtiVassileva@users.noreply.github.com>
2021-03-02 10:42:39 +0200

    Delete Entity Framework Core/EntityRelations directory

--- Entity Framework Core/EntityRelations/P01_StudentSystem/Enums/ContentType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P01_StudentSystem.Enums
-{
-    public enum ContentType
-    {
-        Application = 1, 
-        Pdf = 2, 
-        Zip = 3,
-    }
-}
--- Entity Framework Core/EntityRelations/P03_FootballBetting/Data/Models/Enums/Prediction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P03_FootballBetting.Data.Models.Enums
-{
-    public enum Prediction
-    {
-        HomeTeamWins = 1,
-        AwayTeamWins = 2,
-        Draw = 3
-    }
-}
