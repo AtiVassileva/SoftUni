81af34a2e9bd65025faea2dbe29582110c1f7b67
Ati <65487762+AtiVassileva@users.noreply.github.com>
2021-03-15 11:21:43 +0200

    Add files via upload

---
+++ Entity Framework Core/C#AutoMappingObjects/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            //TODO: Fix the connection string - Configuration.GetConnectionString("DefaultConnection");
+
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=.\\SQLEXPRESS;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
